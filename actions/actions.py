f5a2b3477eb639013098a06a96e27903a75549af
Paul Lorgue <paullorgue@gmail.com>
2021-12-11 15:33:21 +0100

    fix(nlu): add some examples

--- actions/actions.py
+++ actions/actions.py
@@ -9,14 +9,14 @@ import requests
 
 class AskForSlotAction(Action):
     def name(self) -> Text:
-        return "action_ask_jock_category"
+        return "action_ask_joke_category"
 
     def run(
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> List[EventType]:
 
-        want_jocks = tracker.get_slot("want_jocks")
-        if want_jocks:
+        want_jokes = tracker.get_slot("want_jokes")
+        if want_jokes:
             resp = requests.get(url="https://api.chucknorris.io/jokes/categories")
             if(resp.status_code == 200):
                 categories = resp.json()
@@ -31,28 +31,28 @@ class AskForSlotAction(Action):
 
         return []
 
-class DisplayJocks(Action):
+class Displayjokes(Action):
 
     def name(self) -> Text:
-        return "action_display_jocks"
+        return "action_display_jokes"
     
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict):
         
-        category = tracker.get_slot("jock_category")
-        num = tracker.get_slot("num_jocks")
-        want_jocks = tracker.get_slot("want_jocks")
+        category = tracker.get_slot("joke_category")
+        num = tracker.get_slot("num_jokes")
+        want_jokes = tracker.get_slot("want_jokes")
         
-        if want_jocks and num.isdigit() and len(category) > 3:
+        if want_jokes and num.isdigit() and len(category) > 3:
             count = 0
             for i in range(int(num)):
                 resp = requests.get(url=f"https://api.chucknorris.io/jokes/random?category={category}")
                 if(resp.status_code == 200):
-                    jock = resp.json()['value']
-                    dispatcher.utter_message(text=f"{i+1}. {jock}")
+                    joke = resp.json()['value']
+                    dispatcher.utter_message(text=f"{i+1}. {joke}")
                 else:
                     count += 1
             if count == int(num):
-                dispatcher.utter_message(text=f"Sorry but I don't have funny jocks in the {category} theme. Try another :)")
+                dispatcher.utter_message(text=f"Sorry but I don't have funny jokes in the {category} theme. Try another :)")
             else: 
                 dispatcher.utter_message(text="I know, I'm very funny")
         else:
@@ -72,52 +72,52 @@ class ValidateChuckNorrisForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_chucknorris_form"
 
-    def validate_want_jocks(
+    def validate_want_jokes(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        """Validate `want_jocks` value."""
+        """Validate `want_jokes` value."""
 
         want = slot_value
         if want:
-            return {"want_jocks": want}
-        return {"want_jocks":  None}
+            return {"want_jokes": want}
+        return {"want_jokes":  None}
         
 
-    def validate_jock_category(
+    def validate_joke_category(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        """Validate `jock_category` value."""
+        """Validate `joke_category` value."""
 
         # We need an available category.
         category = slot_value
         if category not in self.categories:
             dispatcher.utter_message(text="Please enter a valid theme :)")
-            return {"jock_category": None}
-        return {"jock_category": category}
+            return {"joke_category": None}
+        return {"joke_category": category}
 
-    def validate_num_jocks(
+    def validate_num_jokes(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        """Validate `num_jocks` value."""
+        """Validate `num_jokes` value."""
 
         # We want an integer.
         number = slot_value
         if number.isdigit() :
-            return {"num_jocks": number}
+            return {"num_jokes": number}
         dispatcher.utter_message(text="Please enter a valid number")
-        return {"num_jocks": None}
+        return {"num_jokes": None}
 
 
 ##################
